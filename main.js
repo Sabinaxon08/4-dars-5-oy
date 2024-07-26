c23a4d166851ccc52e00b814d13d7a26f0d1be05
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-07-26 14:47:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// let body = document.querySelector('body')
+
+// setInterval(() => {
+//     let Randomnumber = Math.ceil(Math.random() * 100)
+//     let p = document.createElement('p')
+//     p.innerHTML = ""
+//     body.append(p)
+// }, 1000); 
+
+let p = document.querySelector('#randomson')
+
+p.innerHTML = Math.ceil(Math.random() * 100)
